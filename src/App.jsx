e95ea9bd856d96b1de83402eaec60bfbb23a7977
pblo <pdiazarenas541@gmail.com>
2025-09-06 18:11:10 +0100

    first sketch, second try

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,31 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import MyNavbar from "./components/Navbar.jsx";
 import Home from "./components/Home.jsx";
 import Footer from "./components/Footer.jsx";
+import Register from "./components/Register.jsx";
+import Login from "./components/Login.jsx";
 
 //En Button.jsx se usan props convencioanales, en CardPizza.jsx se usan props via destructuring.
 
 function App() {
+
+  /* Como no está indicado el conectar los botones con las secciones de la app,
+  coloqué este "selector" para observar los contenidos requeridos y aislados.
+  
+  //Solución provisoria 
+  
+  Cambiar el contenido de "visible" a home para llevar al componente home, register para ir al componente register y login para el componente login.
+ */
+  let visible = 'register'
+
   return (
     <>
       <MyNavbar />
-      <Home />
+     { visible === 'home' ? <Home /> : ''} 
+     { visible === 'register' ? <Register /> : ''}
+     { visible === 'login' ? <Login /> : ''} 
       <Footer />
+
+     
     </>
   );
 }
